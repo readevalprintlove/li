2553ffc0195b5cbccf23c1c2adaa5b81ad18ecb6
fogus <mefogus@gmail.com>
2013-03-01 09:18:29 -0500

    Caerbannog's Macros

--- src/K.hs
+++ src/K.hs
@@ -1,5 +1,5 @@
 {-
-Copyright (C) 2012 Michael Fogus <me -at- fogus -dot- me>
+Copyright (C) 2013 Michael Fogus <me -at- fogus -dot- me>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
